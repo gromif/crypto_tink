97f8612bc82af341a8a9220dfdbd781f6205d5d2
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-19 20:41:33 +0200

    Inject Sha256UseCase

--- src/main/java/com/nevidimka655/crypto/tink/extensions/Hash.kt
+++ src/main/java/com/nevidimka655/crypto/tink/extensions/Hash.kt
@@ -3,4 +3,3 @@ package com.nevidimka655.crypto.tink.extensions
 import java.security.MessageDigest
 
 fun String.sha384(): ByteArray = MessageDigest.getInstance("SHA-384").digest(toByteArray())
-fun String.sha256(): ByteArray = MessageDigest.getInstance("SHA-256").digest(toByteArray())
