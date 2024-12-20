d8645e0a6804aea33c0b001e2722c61ae8ed6444
Nevidimka655 <alex.kobrys@gmail.com>
2024-12-20 15:38:36 +0200

    Remove unused extension

--- src/main/java/com/nevidimka655/crypto/tink/extensions/Base64.kt
+++ src/main/java/com/nevidimka655/crypto/tink/extensions/Base64.kt
@@ -4,5 +4,4 @@ import com.google.crypto.tink.subtle.Base64
 
 fun ByteArray.toBase64(): String = Base64.encode(this)
 fun String.toBase64(): String = Base64.encode(toByteArray())
-fun ByteArray.fromBase64(): ByteArray = Base64.decode(decodeToString())
 fun String.fromBase64(): ByteArray = Base64.decode(this)
