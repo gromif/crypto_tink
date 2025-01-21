77be0b2fd1329fdc82920f2d59dc1c7207798c15
gromif <alex.kobrys@gmail.com>
2025-01-21 18:28:58 +0200

    Add WorkerAeadUtil

---
+++ src/main/java/com/nevidimka655/crypto/tink/core/WorkerAeadUtil.kt
@@ -0,0 +1,37 @@
+package com.nevidimka655.crypto.tink.core
+
+import com.google.crypto.tink.Aead
+import com.google.crypto.tink.integration.android.AndroidKeystore
+import com.nevidimka655.crypto.tink.core.encoders.Base64Util
+
+class WorkerAeadUtil(
+    private val base64Util: Base64Util,
+    private val androidKeysetAlias: String,
+    private val associatedData: ByteArray
+) {
+    private var cachedAead: Aead? = null
+
+    fun generateNewKey() {
+        cachedAead = null
+        AndroidKeystore.generateNewAes256GcmKey(androidKeysetAlias)
+    }
+
+    fun deleteKey() {
+        AndroidKeystore.deleteKey(androidKeysetAlias)
+    }
+
+    fun encrypt(data: String): String = encryptBytes(data.toByteArray())
+
+    fun encryptBytes(data: ByteArray): String = base64Util.encode(
+        getAead().encrypt(data, associatedData)
+    )
+
+    fun decrypt(data: String): String = decryptBytes(data).decodeToString()
+
+    fun decryptBytes(data: String): ByteArray = getAead().decrypt(
+        base64Util.decode(data), associatedData
+    )
+
+    private fun getAead() = cachedAead ?: AndroidKeystore.getAead(androidKeysetAlias)
+        .also { cachedAead = it }
+}
