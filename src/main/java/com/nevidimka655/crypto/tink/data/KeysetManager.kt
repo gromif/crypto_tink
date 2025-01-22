81594f427718f840932ce3dd6b79cb8853febc8c
gromif <alex.kobrys@gmail.com>
2025-01-22 21:03:09 +0200

    Synchronize `getKeyset` method

--- src/main/java/com/nevidimka655/crypto/tink/data/KeysetManager.kt
+++ src/main/java/com/nevidimka655/crypto/tink/data/KeysetManager.kt
@@ -6,11 +6,14 @@ import com.google.crypto.tink.Parameters
 import com.google.crypto.tink.config.TinkConfig
 import com.nevidimka655.crypto.tink.domain.KeysetTemplates
 import com.nevidimka655.crypto.tink.domain.keyset.KeysetFactory
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 class KeysetManager(
     private val keysetFactory: KeysetFactory,
     private val associatedDataManager: AssociatedDataManager
 ) {
+    private val mutex = Mutex()
     private val keysetList = SparseArray<KeysetHandle>()
 
     suspend fun aead(aead: KeysetTemplates.AEAD, tag: String = ""): KeysetHandle =
@@ -24,10 +27,11 @@ class KeysetManager(
         associatedData: ByteArray? = null,
         keyParams: Parameters,
         cache: Boolean = true
-    ): KeysetHandle {
+    ): KeysetHandle = mutex.withLock {
         val ad = associatedData ?: associatedDataManager.getAssociatedData()
         val keysetHash = tag.hashCode()
-        return keysetList[keysetHash] ?: keysetFactory.create(
+
+        keysetList[keysetHash] ?: keysetFactory.create(
             tag = tag,
             keyParams = keyParams,
             associatedData = ad
