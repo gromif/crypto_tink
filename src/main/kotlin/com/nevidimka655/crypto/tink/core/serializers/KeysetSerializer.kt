4d890744513429c55d85388fe0f19d448f7fb677
gromif <alex.kobrys@gmail.com>
2025-01-30 16:36:02 +0200

    refactor(tink): move classes to the `kotlin` folder

--- src/main/kotlin/com/nevidimka655/crypto/tink/core/serializers/KeysetSerializer.kt
+++ src/main/kotlin/com/nevidimka655/crypto/tink/core/serializers/KeysetSerializer.kt
@@ -2,7 +2,6 @@ package com.nevidimka655.crypto.tink.core.serializers
 
 import com.google.crypto.tink.InsecureSecretKeyAccess
 import com.google.crypto.tink.KeysetHandle
-import com.google.crypto.tink.SecretKeyAccess
 import com.google.crypto.tink.TinkProtoKeysetFormat
 import com.nevidimka655.crypto.tink.core.encoders.HexUtil
 
