534bb9f3d211b1ded211cf917ca814f01273f388
gromif <alex.kobrys@gmail.com>
2025-01-30 17:06:36 +0200

    refactor(tink): ensure encoders implement `Encoder` model

--- src/main/kotlin/io/gromif/crypto/tink/encoders/Base64Util.kt
+++ src/main/kotlin/io/gromif/crypto/tink/encoders/Base64Util.kt
@@ -2,12 +2,12 @@ package io.gromif.crypto.tink.encoders
 
 import com.google.crypto.tink.subtle.Base64
 
-class Base64Util {
+class Base64Util : Encoder {
 
-    fun encode(bytes: ByteArray): String = Base64.encode(bytes)
-    fun decode(value: String): ByteArray = Base64.decode(value)
+    override fun encode(bytes: ByteArray): String = Base64.encode(bytes)
+    override fun decode(value: String): ByteArray = Base64.decode(value)
 
-    fun encode(value: String): String = encode(bytes = value.toByteArray())
-    fun decode(bytes: ByteArray): ByteArray = decode(value = bytes.decodeToString())
+    override fun encode(value: String): String = encode(bytes = value.toByteArray())
+    override fun decode(bytes: ByteArray): ByteArray = decode(value = bytes.decodeToString())
 
 }
--- src/main/kotlin/io/gromif/crypto/tink/encoders/HexUtil.kt
+++ src/main/kotlin/io/gromif/crypto/tink/encoders/HexUtil.kt
@@ -2,14 +2,22 @@ package io.gromif.crypto.tink.encoders
 
 import com.google.crypto.tink.subtle.Hex
 
-class HexUtil {
+class HexUtil : Encoder {
 
-    fun encode(bytes: ByteArray): String {
+    override fun encode(bytes: ByteArray): String {
         return Hex.encode(bytes)
     }
 
-    fun decode(hex: String): ByteArray {
+    override fun decode(hex: String): ByteArray {
         return Hex.decode(hex)
+    }
+
+    override fun encode(value: String): String {
+        return encode(value.toByteArray())
+    }
+
+    override fun decode(bytes: ByteArray): ByteArray {
+        return decode(bytes.decodeToString())
     }
 
 }
