3f89fa3bd2e4e9149cdf089643823c32981de74b
gromif <alex.kobrys@gmail.com>
2025-02-14 20:37:49 +0200

    refactor(tink): remove unused code

--- src/main/kotlin/io/gromif/crypto/tink/extensions/Base64.kt
+++
@@ -1,7 +0,0 @@
-package io.gromif.crypto.tink.extensions
-
-import com.google.crypto.tink.subtle.Base64
-
-fun ByteArray.toBase64(): String = Base64.encode(this)
-fun String.toBase64(): String = Base64.encode(toByteArray())
-fun String.fromBase64(): ByteArray = Base64.decode(this)
