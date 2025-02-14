83aaa2ed94b912f4ba15fcb9476bd78de32a8277
gromif <alex.kobrys@gmail.com>
2025-02-14 14:43:58 +0200

    refactor(tink): avoid depending on a concrete `Encoder` implementation

--- src/main/kotlin/io/gromif/crypto/tink/core/parsers/KeysetParser.kt
+++ src/main/kotlin/io/gromif/crypto/tink/core/parsers/KeysetParser.kt
@@ -3,16 +3,16 @@ package io.gromif.crypto.tink.core.parsers
 import com.google.crypto.tink.InsecureSecretKeyAccess
 import com.google.crypto.tink.KeysetHandle
 import com.google.crypto.tink.TinkProtoKeysetFormat
-import io.gromif.crypto.tink.encoders.HexEncoder
+import io.gromif.crypto.tink.encoders.Encoder
 
 class KeysetParser(
-    private val hexEncoder: HexEncoder
+    private val encoder: Encoder
 ) {
 
     operator fun invoke(
         serializedKeyset: String
     ): KeysetHandle {
-        val encryptedKeyset = hexEncoder.decode(hex = serializedKeyset)
+        val encryptedKeyset = encoder.decode(value = serializedKeyset)
         return TinkProtoKeysetFormat.parseKeyset(
             /* serializedKeyset = */ encryptedKeyset,
             /* access = */ InsecureSecretKeyAccess.get()
